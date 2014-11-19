e9ad23d903a7fc2edfb24ad5cb3d067a451b5b08
Silviya Sotirova <silviya.soti@gmail.com>
2014-11-19 21:16:37 +0000

    Population script.

---
+++ src/DB/population_script.sql
@@ -0,0 +1,35 @@
+/*ACHIEVEMENT*/
+INSERT INTO ACHIEVEMENT VALUES(0,'No rank','No rank: you have not uploaded yet/many pictures.');
+INSERT INTO ACHIEVEMENT VALUES(1,'Bronze','Bronze: you are uploading pictures and getting points. You are getting a true SunnyGlasgow user!');
+INSERT INTO ACHIEVEMENT VALUES(2,'Silver','Silver: you are uploading pictures very often and getting a lot of points. You are a valuable SunnyGlasgow user!');
+INSERT INTO ACHIEVEMENT VALUES(3,'Gold','Gold: your pictures are outstanding! You are a golden SunnyGlasgow user!');
+INSERT INTO ACHIEVEMENT VALUES(4,'Platinum','Platinum: you are an outstanding SunnyGlasgow user!');
+INSERT INTO ACHIEVEMENT VALUES(5,'Master','Master: your pictures are unique for SunnyGlasgow. You are a master SunnyGlasgow user!');
+/**/
+
+/*RANK*/
+INSERT INTO RANK VALUES(31,155,16);
+INSERT INTO RANK VALUES(32,87,9);
+INSERT INTO RANK VALUES(33,33,2);
+INSERT INTO RANK VALUES(34,10,0);
+INSERT INTO RANK VALUES(35,11,24);
+/**/
+
+
+/*WEATHER*/
+INSERT INTO WEATHER VALUES(61,'Rainy');
+INSERT INTO WEATHER VALUES(62,'Sunny');
+INSERT INTO WEATHER VALUES(63,'Snowy');
+INSERT INTO WEATHER VALUES(64,'Windy');
+/**/
+
+
+/*SYS_USER*/
+INSERT INTO SYS_USER VALUES('user1',31,'pass1','hash1','tom1@gmail.com','ava1',11,'Tom','Robertson');
+INSERT INTO SYS_USER VALUES('user2',32,'pass2','hash2','john2@gmail.com','ava2',12,'John','Wilson');
+INSERT INTO SYS_USER VALUES('user3',33,'pass3','hash3','bay3@gmail.com','ava3',13,'Bay','Kenish');
+INSERT INTO SYS_USER VALUES('user4',34,'pass4','hash4','vasko4@gmail.com','ava4',14,'Vasko','Dimitrov');
+INSERT INTO SYS_USER VALUES('user5',35,'pass5','hash5','lisa5@gmail.com','ava5',15,'Lisa','Eldridge');
+/**/
+
+/*it's still needs work*/
