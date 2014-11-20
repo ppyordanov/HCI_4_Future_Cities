c1729eb460b9f073bd11079649884e30cba4ed7b
ppyordanov <ppyordanov@yahoo.com>
2014-11-20 18:46:18 +0000

    [VIRTUALENV] added to be used with requirements.txt

---
+++ src/activate/Scripts/activate.bat
@@ -0,0 +1,26 @@
+@echo off
+set "VIRTUAL_ENV=C:\Users\Peter Yordanov\Desktop\Ppyordanov's files\GU_Files_2014_15_lvl4\HCI4\Assessed_Exercises\HCI_4_Future_Cities\src\activate"
+
+if defined _OLD_VIRTUAL_PROMPT (
+    set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
+) else (
+    if not defined PROMPT (
+        set "PROMPT=$P$G"
+    )
+	set "_OLD_VIRTUAL_PROMPT=%PROMPT%"	
+)
+set "PROMPT=(activate) %PROMPT%"
+
+if not defined _OLD_VIRTUAL_PYTHONHOME (
+    set "_OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%"
+)
+set PYTHONHOME=
+
+if defined _OLD_VIRTUAL_PATH (
+    set "PATH=%_OLD_VIRTUAL_PATH%"
+) else (
+    set "_OLD_VIRTUAL_PATH=%PATH%"
+)
+set "PATH=%VIRTUAL_ENV%\Scripts;%PATH%"
+
+:END
